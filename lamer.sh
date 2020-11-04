66f8502bd5e340405475b2b8ab93f548d2399b76
zooleen <zooleen@gmail.com>
2020-11-04 18:09:04 +0500

    Added comments

--- lamer.sh
+++ lamer.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# This script takes a screen capture from list of webcameras IPs
+# Maked for one guy from tg:@srv_admins
+# Usage: ./lamer.sg <list_of_IPs>.txt
+# For local usage only
+# Copyright Ivan Doobro 2020
 
 if [ -z "$1" ]
     then
