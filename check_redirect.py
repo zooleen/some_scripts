26f553f68d4efcd6504312dc51de25d56ba4f98f
Дубровин Иван Андреевич <dubrovin.ivan@banklab.tech>
2020-11-16 16:33:38 +0500

    Added usage info to check_redirect.py

--- check_redirect.py
+++ check_redirect.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 #
-# This script examing argument host for redirecting to ezaem
+# This script examing <host> from first argument
+# for redirecting to a link that include <expression>
+# from second argument.
+#
+# Returns 0 if ok, else return 1.
+#
+# Usage: ./check_redirect.py <some_host> <expression>
+#
+# Ivan Doobro 2020
 
 from sys import argv
 from sys import exit
 from os import popen
 import re
 
-scriptName, targetHost = argv
+scriptName, targetHost, expression = argv
 
 ext = popen("curl -Ls -o /dev/null -w %{url_effective} " + targetHost + "/click/1/1")
 # Amazing code <3
@@ -15,9 +23,7 @@ for i in ext:
     myStr = i
     break
 
-print(myStr)
-
-if (re.search('ezaem', myStr) == None):
+if (re.search(expression, myStr) == None):
     exit(1)
 else:
     exit(0)
